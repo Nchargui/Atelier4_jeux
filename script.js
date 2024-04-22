131eb8318c2868c0c1c307edb010f5c0ac81196c
Aya Issa <2139923@collegemv.qc.ca>
2024-04-22 17:08:34 -0400

    Ajout background & commentaires

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
 const app = new PIXI.Application();
-await app.init({ width: 640, height: 360 });
+await app.init({ width: 1900, height: 850 });
 document.body.appendChild(app.canvas);
 
 
 
-await PIXI.Assets.load('plateform.png');
-let sprite = PIXI.Sprite.from('plateform.png');
+await PIXI.Assets.load('background.png');
+let sprite = PIXI.Sprite.from('background.png');
+
+sprite.width = app.screen.width;
+sprite.height = app.screen.height;
+
+sprite.anchor.set(0.5);
+
+
+sprite.position.set(app.screen.width /2, app.screen.height/2)
 
 
 app.stage.addChild(sprite);
+
+
 
 
