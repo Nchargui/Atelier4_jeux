85ce3a7ae932c7ed2d59a70046c5e2f8d53253dc
Nadine Chargui <2283110@collegemv.qc.ca>
2024-04-24 11:57:29 -0400

    sprite player

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ const view = canvas.transferControlToOffscreen();
 
     
     const app = new PIXI.Application();
+
+    let keys = {};
    
 
     await app.init({view, resizeTo:window});
@@ -22,13 +24,19 @@ const view = canvas.transferControlToOffscreen();
     await Assets.load(['img/3.png']);
     await Assets.load(['img/4.png']);
     await Assets.load(['img/tiles.png']);
+    await Assets.load(['img/image.png']);
+
+   
 
 
     const background = Sprite.from('img/1.png')
     const clouds1 = Sprite.from('img/3.png')
     const clouds2 = Sprite.from('img/4.png')
     const tiles = Sprite.from('img/tiles.png')
+    const weird = Sprite.from('img/image.png')
 
+
+    
    
 
 
@@ -36,29 +44,47 @@ const view = canvas.transferControlToOffscreen();
 
 
     background.width = width * 2.7;
-    background.height = height;
+    background.height = height * 2;
     
-    clouds1.width = width * 2.7;
-    clouds1.height = height;
+    clouds1.width = width *  2.7;
+    clouds1.height = height
 
-    clouds2.width = width * 2.7;
-    clouds2.height = height;
-
-    tiles.width +=0;
-    tiles.height +=50;
+    clouds2.width = width *  2.7;
+    clouds2.height = height
 
 
-    tiles.y += 500;
-    
+    tiles.height = height * 1;
+    tiles.width = width * 4.5;
 
-    
-
-
+    tiles.y += 5;
 
 
-    app.stage.addChild(background, clouds1, clouds2, tiles);
+   weird.anchor.set(0.5);
+   weird.x = app.view.width / 2;
+   weird.y = app.view.height / 2;
+
+   
+    app.stage.addChild(background, clouds1, clouds2, tiles, weird);
 
 
+    window.addEventListener("keydown", keysDown);
+    window.addEventListener("keyup", keysUp);
+
+    function keysDown(e){
+        console.log(e.keyCode);
+        keys[e.keyCode] = true;
+
+
+    }
+
+    function keysUp(e){
+        console.log(e.keyCode);
+        keys[e.keyCode] = false;
+
+    }
+
+
+   
   
 
 
