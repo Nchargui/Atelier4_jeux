1363177625a8b7386110f1134e6c87d65336f4d2
Nadine Chargui <nnadine.chargui@gmail.com>
2024-04-27 10:39:18 -0400

    fix of bg that changed everytime we refreshed

--- script.js
+++ script.js
@@ -1,91 +1,79 @@
-import { Application, Assets, Graphics, Sprite, RenderTexture, Point} from 'https://cdn.skypack.dev/pixi.js'; 
+import {
+  Application,
+  Assets,
+  Graphics,
+  Sprite,
+  RenderTexture,
+  Point,
+} from "https://cdn.skypack.dev/pixi.js";
 
 // Lancement de l'application +++++++++++++++++
-const canvas = document.createElement('canvas');
-canvas.width = window.innerWidth;
-canvas.height = window.innerHeight;
-const view = canvas.transferControlToOffscreen();
+const canvas = document.createElement("canvas");
+const appWidth = 1900;
+const appHeight = 900;
+const app = new PIXI.Application({ view: canvas });
 
-(async () =>{
+(async () => {
+  await app.init();
+  app.renderer.resize(appWidth, appHeight);
+  document.body.appendChild(app.view);
 
-    const app = new PIXI.Application({
-        view
-    });
+  // load the sprites  +++++++++++++++++
 
+  await Assets.load([
+    "img/1.png",
+    "img/3.png",
+    "img/4.png",
+    "img/tiles.png",
+    "img/image.png",
+  ]);
 
-    await app.init();
-    app.renderer.resize(window.innerWidth, window.innerHeight);
-    document.body.appendChild(app.view);
+  const background = Sprite.from("img/1.png");
+  const clouds1 = Sprite.from("img/3.png");
+  const clouds2 = Sprite.from("img/4.png");
+  const tiles = Sprite.from("img/tiles.png");
+  const player1 = Sprite.from("img/image.png");
 
-    
+  // give the spirtes their inital placement +++++++++++++++++
 
+  background.width = appWidth; //background ======
+  background.height = appHeight;
 
+  clouds1.width = appWidth;
+  clouds1.height = appHeight;
 
-// load the sprites  +++++++++++++++++
-
-    await Assets.load(['img/1.png', 'img/3.png', 'img/4.png', 'img/tiles.png', 'img/image.png']);
-   
-    const background = Sprite.from('img/1.png')
-    const clouds1 = Sprite.from('img/3.png')
-    const clouds2 = Sprite.from('img/4.png')
-    const tiles = Sprite.from('img/tiles.png')
-    const player1 = Sprite.from('img/image.png')
-
-
-
-   // give the spirtes their inital placement +++++++++++++++++
-
-    background.width = canvas.width  //background ======
-    background.height = canvas.height;
-    
-    clouds1.width = canvas.width
-    clouds1.height = canvas.height;
-
-    clouds2.width = canvas.width;
-    clouds2.height = canvas.height;
-
-    tiles.height = canvas.height;
-    tiles.width = canvas.width * 2.5;
-    tiles.y += 50;
+  clouds2.width = appWidth;
+  clouds2.height = appHeight;
 
 
-   player1.scale.set(2);
-   player1.x = canvas.width / 2;
-   player1.y = canvas.height / 2 ;
-   
+  tiles.height = appWidth;
+  tiles.width = appHeight;
+  tiles.y += 50;
 
-   //bring up the sprites
-    app.stage.addChild(background, clouds1, clouds2, tiles, player1);
+  player1.scale.set(2);
+  player1.x = canvas.width / 2;
+  player1.y = canvas.height / 2;
 
-    // class for the box of player
-    // class Box {
-    //     constructor(player)
-    // }
+  //bring up the sprites
+  app.stage.addChild(background, clouds1, clouds2, tiles, player1);
 
-    ///// GRAVITY //////
+  // class for the box of player
+  // class Box {
+  //     constructor(player)
+  // }
 
-    let gravity = new Point(0, 0.5); // Adjust the Y value to control gravity strength
+  ///// GRAVITY //////
 
-    function applyGravity() {
-        player1.y += gravity.y;
-        
-    } 
+  let gravity = new Point(0, 0.5); // Adjust the Y value to control gravity strength
 
+  function applyGravity() {
+    player1.y += gravity.y;
+  }
 
-    function animate(){
-        applyGravity()
-        window.requestAnimationFrame(animate)
-    }
+  function animate() {
+    applyGravity();
+    window.requestAnimationFrame(animate);
+  }
 
-
-    animate()
-
-
-
-   
-    
-
-
-
-
- })();
+  animate();
+})();
