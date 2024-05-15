d95e7508eb66e59f93a3a4e1e9d66b82dc8ed235
AyaIssa1 <2139923@collegemv.qc.ca>
2024-05-15 19:31:13 -0400

    ajout clouds

--- script.js
+++ script.js
@@ -6,23 +6,27 @@ const ASSETS = {
 
 
 
-IMAGE:{
+    IMAGE: {
 
-    TREE:{
-        src:"/img/palm.png",
-        width: 132,
-        height: 192,
-    },
+        TREE: {
+            src: "/img/palm.png",
+            width: 132,
+            height: 192,
+        },
 
-    CAR:{
-        src:"/img/Car.png",
-        width: 132,
-        height: 192,
+        CAR: {
+            src: "/img/Car.png",
+            width: 132,
+            height: 192,
+        }
+
+
+
     }
-
-    
-
-} 
 
 
 }
+
+
+
+
