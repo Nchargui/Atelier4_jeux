b3c2ecb3e1f4f4ba63827b4ce8858135dfca5b1f
Aya Issa <2139923@collegemv.qc.ca>
2024-05-16 09:17:48 -0400

    aucun ajout

--- script.js
+++ script.js
@@ -1,11 +1,14 @@
-// assets 
 
 
 
 const ASSETS = {
 
 
-
+    COLOR: {
+        TAR: ["#959298", "#9c9a9d"],
+        RUMBLE: ["#959298", "#f5f2f6"],
+        GRASS: ["#eedccd", "#e6d4c5"],
+    },
     IMAGE: {
 
         TREE: {
