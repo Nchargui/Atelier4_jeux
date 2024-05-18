7e1d0431a85b081bfa66500b2bd5d727e2eef6b6
Nadine Chargui <nnadine.chargui@gmail.com>
2024-05-18 16:05:57 -0400

    ajout du canva, de la voiture, les controlles de la mette avec control API

---
+++ script.js
@@ -0,0 +1,140 @@
+const canvas = document.getElementById("RacingGame");
+const context = canvas.getContext("2d");
+
+/// inisialiser la taille des assets et jeux /////////////
+let carWidthAndHeight = 0; 
+let carX = 0;
+let carY = 0;
+let velocity = 0
+///////////////////////////////////////
+
+
+///Les controles///////////////////////
+let controllerIndex = null;
+let GoingLeft = false;
+let GoingRight = false;
+let VibrateController = false;
+let GoingUp = false;
+///////////////////////////////////////
+
+
+function setUpRacingGame(){  // on veut que le jeux, fit les proportions de l'écrans
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    carWidthAndHeight = canvas.width * 0.1;
+    velocity = canvas.width * 0.01;
+
+    carX = (canvas.width - carWidthAndHeight)/2;
+    carY = (canvas.height - carWidthAndHeight)/2;
+
+}
+
+window.addEventListener('resize', setUpRacingGame);  //screen adapté à la fenêtre
+
+
+
+/// Utilisation de Gamepad API///////////////////////////////////////////3
+window.addEventListener('gamepadconnected', (event) =>{  // si la manette est connecter
+    controllerIndex = event.gamepad.index;
+    console.log("La manette est connecté")
+}); 
+
+window.addEventListener('gamepaddisconnected', (event) =>{ //si la manette est deconnecter
+    console.log("La manette est deconnecté");
+    controllerIndex = null;
+    
+
+}); 
+
+///////////////////////////////////////////3
+
+// Inisitaliser les assets du jeux///////////////////////////////////////
+let backgroundSky =  new Image();
+backgroundSky.src  = "img/1.png";  // on défine le background de notre jeux (le ciel)
+
+let carPlayer = new Image();  // création de la voiture
+carPlayer.src = "img/Car.png";  // on va charger la voiture dans le jeux
+///////////////////////////////////////
+
+
+
+///// Load les assets /////////////////////////////////////
+
+backgroundSky.onload = function(){
+setUpRacingGame();
+gameLoop()
+}
+
+
+function clearScreen(){
+    context.drawImage(backgroundSky, 0, 0, canvas.width, canvas.height);
+}
+
+
+function drawCar(){
+    context.drawImage(carPlayer, carX, carY * 1.5, carWidthAndHeight * 1.5, carWidthAndHeight * 1.5);
+}
+
+///////////////////////////////////////
+
+
+////Faire bouger la voiture et autre controle//////////////////////////////////
+function controllerCar(){
+    if(controllerIndex !== null){
+        const gamepad = navigator.getGamepads()[controllerIndex];
+        const buttons = gamepad.buttons;
+        GoingLeft = buttons[14].pressed;
+        GoingRight = buttons[15].pressed;
+        VibrateController = buttons[13].pressed;
+        GoingUp = buttons[12].pressed;
+
+
+        if(VibrateController){
+            gamepad.vibrationActuator.playEffect("dual-rumble", {  // partie prit de chatGpt pour faire vibrer la manette 
+                startDelay: 0,
+                duration: 100,
+                weakMagnitude: 0.5,
+                strongMagnitude: 0.5,
+            });
+
+        }
+
+
+        
+    }   
+
+}
+
+function moveCar(){
+    if(GoingLeft){
+        carX -= velocity
+    }
+
+    if(GoingRight){
+        carX += velocity
+    }
+
+    
+    if(GoingUp){
+        carY -= velocity;
+    }
+
+}
+
+function updateMovesCar(){
+    moveCar();
+}
+
+
+/// loop pour le jeux
+function gameLoop(){
+    console.log("la boucle marche !")
+    clearScreen();
+    drawCar();
+    controllerCar();
+    updateMovesCar();
+    requestAnimationFrame(gameLoop);
+}
+
+
+
