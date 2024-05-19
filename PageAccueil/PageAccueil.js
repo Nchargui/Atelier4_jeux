18de6c7bfc51765d08729f6b2a8b38450e214742
AyaIssa1 <2139923@collegemv.qc.ca>
2024-05-19 15:04:48 -0400

    liens entre la page accueil et page choisir auto

---
+++ PageAccueil/PageAccueil.js
@@ -0,0 +1,5 @@
+//Button start --> Page choisir la voiture
+document.getElementById("pressStartButton").addEventListener("click", function() {
+    window.location.href = "PageChoisirVoiture.html";
+});
+
