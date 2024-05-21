56c1e1ba25d3c32a9c8102d4be356d9fc975b088
AyaIssa1 <2139923@collegemv.qc.ca>
2024-05-21 10:08:49 -0400

    ajout element

--- PageAccueil/PageAccueil.js
+++ PageAccueil/PageAccueil.js
@@ -1,5 +1,3 @@
-
-
 //Button start --> Page choisir la voiture
 document.getElementById("pressStartButton").addEventListener("click", function() {
     window.location.href = "PageChoisirVoiture.html";
