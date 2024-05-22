9b18bb84f74cee184f9663c258687cb92a10187c
Nadine Chargui <nnadine.chargui@gmail.com>
2024-05-21 23:10:36 -0400

    spawn les ennemies (voitures), apparait vaiment n'importe comment, à régler

--- script.js
+++ script.js
@@ -6,6 +6,15 @@ let carWidthAndHeight = 0;
 let carX = 0;
 let carY = 0;
 let velocity = 0
+
+const enemyCars = [];
+const enemyCarCount =  4
+const enemyCarSize = 0.40;
+
+let enemyCarVelocity = 5;
+
+
+
 ///////////////////////////////////////
 
 
@@ -17,8 +26,15 @@ let VibrateController = false;
 let GoingUp = false;
 ///////////////////////////////////////
 
+//Vriables pour les animations
 let cloud1X = 0;
 let cloud1Velocity = 1;
+
+
+let frameSeconde = 30;
+let intervalFrame = 20;
+let lastFrameTime = 0;
+///////////////////////////////////////
 
 
 function setUpRacingGame() {  // on veut que le jeux, fit les proportions de l'écrans
@@ -30,6 +46,37 @@ function setUpRacingGame() {  // on veut que le jeux, fit les proportions de l'�
     carX = (canvas.width - carWidthAndHeight) / 2;
     carY = (canvas.height - carWidthAndHeight) / 2;
 
+
+    enemyCarX = canvas.width / 3;
+    enemyCarY = canvas.height / 2;
+    enemyCarWidth = carWidthAndHeight / 6;
+    enemyCarHeight = carWidthAndHeight / 6;
+    enemyCarVelocity = velocity * 2;
+
+
+
+}
+
+
+function infiniteSpawnCar() {  // aide AI
+    while (enemyCars.length < enemyCarCount) {
+        let size = carWidthAndHeight * enemyCarSize;
+        let roadWidth = canvas.width * 0.8;
+        let roadStartX = (canvas.width - roadWidth) / 2;
+        let roadTopY = RoadFrames[currentFrame].height * 0.2;
+        let roadBottomY = canvas.height * 0.8;
+        let spawnX = roadStartX + Math.random() * roadWidth;
+        let spawnY = roadTopY - size / 2; 
+
+        let newenemyCars = {
+            x: spawnX,
+            y: spawnY,
+            width: size,
+            height: size,
+            velocityY: enemyCarVelocity
+        };
+        enemyCars.push(newenemyCars);
+    }
 }
 
 window.addEventListener('resize', setUpRacingGame);  //screen adapté à la fenêtre
@@ -56,7 +103,7 @@ let backgroundSky = new Image();
 backgroundSky.src = "img/1.png";  // on défine le background de notre jeux (le ciel)
 
 let Floor = new Image();
-Floor.src = "img/FloorRace.png";
+Floor.src = "img/Desert.png";
 
 
 let Cloud1 = new Image();
@@ -65,6 +112,9 @@ Cloud1.src = "img/cloud1.png";
 
 let carPlayer = new Image();  // création de la voiture
 carPlayer.src = "img/Car.png";  // on va charger la voiture dans le jeux
+
+let carEnemy = new Image();  // création de la voiture
+carEnemy.src = "img/carEnemy3.png";
 
 
 ///////////////////////////////////////
@@ -79,7 +129,7 @@ backgroundSky.onload = function () {
 }
 
 
-function clearScreen() {
+function clearScreen() { // load les nuages + l'animation
     context.drawImage(backgroundSky, 0, 0, canvas.width, canvas.height);
 }
 
@@ -98,23 +148,31 @@ function DrawCloud1() {
 }
 
 
-function DrawFloor() {
-    context.drawImage(Floor, 0, 0, canvas.width, canvas.height);
+function DrawFloor() {  // load le deserts (background)
+    context.drawImage(Floor, 0, -90, canvas.width, canvas.height);
 }
 
 
-function drawCar() {
+function drawCar() {  // load la voiture du joueur
     context.drawImage(carPlayer, carX, carY * 1.5, carWidthAndHeight * 1.5, carWidthAndHeight * 1.5);
 }
 
 
+function drawCarEnemy() {  // load la voiture du joueur
+    enemyCars.forEach(car => {
+        context.drawImage(carEnemy, car.x, car.y, car.width, car.height)
+    });
+}
 
+
+
+/// liste pour rentrer toutes les frames
 let RoadFrames = [];
 let currentFrame = 0;
 function LoadFloorGif() {
 
   
-    for (let i = 0; i <= 25; i++) {
+    for (let i = 0; i <= 24; i++) {
         const filename = `img/RoadImgs/frame_${i < 10 ? '0' : ''}${i}_delay-0.04s.gif`;
 
         const RoadGif = new Image();
@@ -127,7 +185,6 @@ function LoadFloorGif() {
 function drawRoadFrame(){
     context.drawImage(RoadFrames[currentFrame], 0, 0, canvas.width, canvas.height);
 
-    currentFrame = (currentFrame + 1) % RoadFrames.length;
     currentFrame++;
 }   
 
@@ -173,24 +230,54 @@ function moveCar() {
 
 }
 
+
 function updateMovesCar() {
     moveCar();
 }
 
 
+function updateEnemyCar(){  // controler l'avancement des voitures ennemies
+    enemyCars.forEach(car => {
+        car.y += car.velocityY; // Changed to move cars downwards
+        if (car.y > canvas.height) { // If the car goes below the canvas
+            car.x = Math.random() * (canvas.width - car.width);
+            car.y = -car.height; // Respawn above the canvas
+        }
+    });
+}
+///////////////////////////////////////
+
+
 /// loop pour le jeux
-function gameLoop() {
-    console.log("la boucle marche !")
+function gameLoop(timestamp) {  // controller la vitesse
+    console.log("la boucle du jeu marche !")
+
+    if(timestamp - lastFrameTime < intervalFrame){
+        requestAnimationFrame(gameLoop);
+        return;
+    }
+    lastFrameTime = timestamp;
+    
     clearScreen();
     DrawCloud1()
     DrawFloor();
     LoadFloorGif();
-    drawRoadFrame()
+    drawRoadFrame();
+    drawCarEnemy();
+    updateEnemyCar();
     drawCar();
     controllerCar();
     updateMovesCar();
+    infiniteSpawnCar();
+
     requestAnimationFrame(gameLoop);
+
 }
+
+
+
+
+    
 
 
 
