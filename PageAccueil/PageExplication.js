6d08faef5c48a8509c7e0073ba191421edfe2bac
AyaIssa1 <2139923@collegemv.qc.ca>
2024-05-21 22:17:24 -0400

    ajout de la langue dans la page Explication + changement du bouton mute et unmute pour un icone

--- PageAccueil/PageExplication.js
+++ PageAccueil/PageExplication.js
@@ -1,26 +1,67 @@
 //Button How to play --> Page Accueil
-document.getElementById("pressBack").addEventListener("click", function() {
-    window.location.href = "PageAccueil.html";
-});
-
+document.addEventListener('DOMContentLoaded', () => {
+  const backButton = document.getElementById("pressBack");
+  if (backButton) {
+      backButton.addEventListener("click", function() {
+          window.location.href = "PageAccueil.html";
+      });
+  }
 
 //audio
-document.addEventListener('DOMContentLoaded', () => {
-    const audio = document.getElementById('myAudio');
-    audio.play();
-  
-    const muteButton = document.getElementById('muteButton');
-    let isMuted = false;
-  
-    muteButton.addEventListener('click', () => {
-      if (isMuted) {
-        audio.play();
-        muteButton.innerText = 'Mute';
-      } else {
-        audio.pause();
-        muteButton.innerText = 'Unmute';
-      }
-      isMuted = !isMuted;
-    });
+
+const audio = document.getElementById('myAudio');
+audio.play();
+
+const muteButton = document.getElementById('muteButton');
+let isMuted = false;
+
+function updateMuteButtonIcon() {
+    if (isMuted) {
+        muteButton.innerHTML = '<i class="fas fa-volume-mute"></i>';
+    } else {
+        muteButton.innerHTML = '<i class="fas fa-volume-up"></i>';
+    }
+}
+
+muteButton.addEventListener('click', () => {
+  if (isMuted) {
+      audio.play();
+  } else {
+      audio.pause();
+  }
+  isMuted = !isMuted;
+  updateMuteButtonIcon();
 });
-  
+
+updateMuteButtonIcon();
+
+// Traduction
+fetch('Json/traduction.json')
+  .then(response => response.json())
+  .then(translations => {
+      function changeLanguage(lang) {
+          const elements = document.querySelectorAll('[data-translate]');
+          elements.forEach(element => {
+              const key = element.getAttribute('data-translate');
+              element.textContent = translations[lang][key];
+          });
+          sessionStorage.setItem('language', lang);
+
+          updateMuteButtonIcon();
+      }
+
+      const languageButtons = document.querySelectorAll('.language-button');
+      languageButtons.forEach(button => {
+          button.addEventListener('click', () => {
+              const lang = button.getAttribute('data-lang');
+              changeLanguage(lang);
+          });
+      });
+
+      const savedLanguage = sessionStorage.getItem('language');
+      if (savedLanguage) {
+          changeLanguage(savedLanguage);
+      }
+  })
+  .catch(error => console.error('Error loading translations:', error));
+});
